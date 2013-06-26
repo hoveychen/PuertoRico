78981638215a4069c446852157212a132c69a8e6
chenyuheng <chenyuheng@google.com>
2013-06-27 00:46:23 +0800

    Refactor file structure

--- PuertoRico/build/source/buildConfig/debug/com/eternizedlab/puertorico/BuildConfig.java
+++ PuertoRico/build/source/buildConfig/debug/com/eternizedlab/puertorico/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.eternized_lab.pr;
+package com.eternizedlab.puertorico;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- PuertoRico/build/source/r/debug/com/eternizedlab/puertorico/R.java
+++ PuertoRico/build/source/r/debug/com/eternizedlab/puertorico/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.eternized_lab.pr;
+package com.eternizedlab.puertorico;
 
 public final class R {
     public static final class attr {
